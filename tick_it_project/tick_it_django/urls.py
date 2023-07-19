a3c837694803113516f923d3a2263f7790a90e18
Elliot <ellthrowaway2@icloud.com>
2023-07-19 16:18:26 -0400

    push to trigger new railway deploy

--- tick_it_project/tick_it_django/urls.py
+++ tick_it_project/tick_it_django/urls.py
@@ -1,6 +1,6 @@
 
 """
-URL configuration for tick_it_django project.
+URL configuration for tick_it_django project
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
