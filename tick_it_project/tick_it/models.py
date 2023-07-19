160a6a3b661fce90fe111642da2276d354c3ce3f
Elliot <ellthrowaway2@icloud.com>
2023-07-19 11:01:36 -0400

    set up models for venue and event; changed name from 'artist' to 'event' in admin.py

--- tick_it_project/tick_it/models.py
+++ tick_it_project/tick_it/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Venue(models.Model):
+    venue_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    description = models.TextField(max_length=300)
+    url = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.venue_name
+
+class Event(models.Model):
+    event_name =  models.CharField(max_length=100)
+    venue_name = models.ForeignKey(Venue, on_delete=models.CASCADE, related_name='events')
+    event_type = models.CharField(max_length=100)
+    event_description = models.TextField(max_length=300)
+    event_date = models.CharField(max_length=50)
+    event_time = models.CharField(max_length=50)
+    ticket_price = models.PositiveIntegerField()
+    ticket_quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.event_name
