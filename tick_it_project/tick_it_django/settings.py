3eb51b8cab8e3406ff78abeba947e49c37d69390
Elliot <ellthrowaway2@icloud.com>
2023-07-20 11:42:57 -0400

    removed reqs txt & procfile

--- tick_it_project/tick_it_django/settings.py
+++ tick_it_project/tick_it_django/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
