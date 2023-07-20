b792d48bf838a2b85c4b45ae54cfdeef98b8c2dc
Elliot <ellthrowaway2@icloud.com>
2023-07-20 11:13:11 -0400

    set debug to false

--- tick_it_project/tick_it_django/settings.py
+++ tick_it_project/tick_it_django/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
