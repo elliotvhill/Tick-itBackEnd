7137e76dab2745e0e2780e1accb334781ef02e91
Elliot <ellthrowaway2@icloud.com>
2023-07-21 00:23:54 -0400

    added ids

--- tick_it_project/tick_it/models.py
+++ tick_it_project/tick_it/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 class Venue(models.Model):
     venue_name = models.CharField(max_length=100)
-    
     address = models.CharField(max_length=100)
     description = models.TextField(max_length=300)
     url = models.CharField(max_length=100)
