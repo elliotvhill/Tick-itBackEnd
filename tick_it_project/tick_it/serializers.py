7a13089046f908bc149fea8a092a726504b4683d
Elliot <ellthrowaway2@icloud.com>
2023-07-20 22:01:00 -0400

    fixed fields issues

--- tick_it_project/tick_it/serializers.py
+++ tick_it_project/tick_it/serializers.py
@@ -2,24 +2,38 @@ from rest_framework import serializers
 from .models import Event, Venue
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
-   
+    venue = serializers.HyperlinkedRelatedField(
+        view_name='venue_name',
+        read_only=True
+    )
+
+    # venue_id = serializers.PrimaryKeyRelatedField(
+    #     queryset=Venue.objects.all(),
+    #     source='venue'
+    # )
  
     class Meta:
         model = Event
-        fields = ('event_name', 'venue_name', 'event_type', 'event_description', 'event_date', 'event_time', 'ticket_price', 'ticket_quantity' )
+        fields = ('__all__')
+        # fields = ('event_name', 'venue_name', 'event_type', 'event_description', 'event_date', 'event_time', 'ticket_price', 'ticket_quantity' )
 
 
 
 class VenueSerializer(serializers.HyperlinkedModelSerializer):
     events = EventSerializer(
-        many = True,
-        read_only = True
+        many=True,
+        read_only=True
+    )
+
+    venue_url = serializers.ModelSerializer.serializer_url_field(
+        view_name='venue-detail'
     )
 
 
     class Meta:
         model = Venue
-        fields = ( 'venue_name', 'address', 'description', 'url', 'events')
+        fields = ('__all__')
+        # fields = ( 'venue_name', 'address', 'description', 'venue_url', 'url', 'events')
 
 
 
--- tick_it_project/tick_it_django/settings.py
+++ tick_it_project/tick_it_django/settings.py
@@ -42,12 +42,21 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tick_it',
     'rest_framework',
+    'corsheaders'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:5173",
+    "http://127.0.0.1:5555",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
